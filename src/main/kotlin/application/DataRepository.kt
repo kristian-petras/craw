03ea2d41e919331ab0c35dba7ff1117a3548649a
Kristian Petras <kristian.petras@second-foundation.eu>
2023-07-09 12:41:20 +0200

    Added modify record functionality

--- src/main/kotlin/application/DataRepository.kt
+++ src/main/kotlin/application/DataRepository.kt
@@ -5,4 +5,5 @@ import model.WebsiteRecord
 interface DataRepository {
     fun getWebsiteRecords(): List<WebsiteRecord>
     fun addWebsiteRecord(record: WebsiteRecord) : Boolean
+    fun modifyWebsiteRecord(website: WebsiteRecord) : Boolean
 }
--- src/main/kotlin/application/LocalDataRepository.kt
+++ src/main/kotlin/application/LocalDataRepository.kt
@@ -3,7 +3,15 @@ package application
 import model.WebsiteRecord
 
 class LocalDataRepository : DataRepository {
+    // Think about making it a map.
     private val websiteRecords = mutableListOf<WebsiteRecord>()
     override fun getWebsiteRecords(): List<WebsiteRecord> = websiteRecords
     override fun addWebsiteRecord(record: WebsiteRecord) = websiteRecords.add(record)
+    override fun modifyWebsiteRecord(website: WebsiteRecord): Boolean {
+        val success = websiteRecords.removeIf { website.url == it.url }
+        if (success) {
+            websiteRecords.add(website)
+        }
+        return success
+    }
 }
