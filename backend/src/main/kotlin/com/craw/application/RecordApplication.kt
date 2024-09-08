f07f0bcb1404ec61dc91498f874dd6ba49456add
tino <kpetras@tutanota.com>
2024-09-08 17:34:19 +0200

    added record application implementation and repository skeleton

--- backend/src/main/kotlin/com/craw/application/RecordApplication.kt
+++ backend/src/main/kotlin/com/craw/application/RecordApplication.kt
@@ -1,14 +1,64 @@
 package com.craw.application
 
+import com.craw.schema.internal.RecordCreate
+import com.craw.schema.internal.RecordUpdate
 import com.craw.schema.rest.WebsiteRecord
 import com.craw.schema.rest.WebsiteRecordCreate
 import com.craw.schema.rest.WebsiteRecordDelete
 import com.craw.schema.rest.WebsiteRecordUpdate
+import com.craw.translator.RestTranslator
+import kotlin.time.Duration
 
-interface RecordApplication {
-    fun getAll(): List<WebsiteRecord>
-    fun get(id: String): WebsiteRecord?
-    fun post(record: WebsiteRecordCreate): String
-    fun put(record: WebsiteRecordUpdate): Boolean
-    fun delete(record: WebsiteRecordDelete): Boolean
+class RecordApplication(
+    private val translator: RestTranslator,
+    private val repository: Repository,
+    private val executor: Executor,
+) {
+    fun getAll(): List<WebsiteRecord> {
+        val records = repository.records()
+        return records.map { translator.translate(it) }
+    }
+
+    fun get(id: String): WebsiteRecord? {
+        val record = repository.record(id) ?: return null
+        return translator.translate(record)
+    }
+
+    fun post(record: WebsiteRecordCreate): String {
+        val newRecord = translator.translate(record)
+        val recordId = repository.create(newRecord)
+
+        val schedule = newRecord.toSchedule(recordId)
+        executor.schedule(schedule)
+
+        return recordId
+    }
+
+    fun put(record: WebsiteRecordUpdate): Boolean {
+        val updatedRecord = translator.translate(record)
+
+        val schedule = updatedRecord.toSchedule()
+        executor.remove(record.recordId)
+        executor.schedule(schedule)
+
+        return repository.update(updatedRecord)
+    }
+
+    fun delete(record: WebsiteRecordDelete): Boolean {
+        return repository.delete(record.recordId)
+    }
+
+    private fun RecordCreate.toSchedule(id: String): ExecutionSchedule = ExecutionSchedule(
+        recordId = id,
+        baseUrl = this.baseUrl,
+        regexp = this.regexp.toRegex(),
+        periodicity = Duration.parse(this.periodicity),
+    )
+
+    private fun RecordUpdate.toSchedule(): ExecutionSchedule = ExecutionSchedule(
+        recordId = this.recordId,
+        baseUrl = this.baseUrl,
+        regexp = this.regexp.toRegex(),
+        periodicity = Duration.parse(this.periodicity),
+    )
 }
--- backend/src/main/kotlin/com/craw/application/Repository.kt
+++ backend/src/main/kotlin/com/craw/application/Repository.kt
@@ -1,9 +1,35 @@
 package com.craw.application
 
 import com.craw.schema.internal.Record
+import com.craw.schema.internal.RecordCreate
+import com.craw.schema.internal.RecordUpdate
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
 
-class Repository {
+class Repository(private val database: Database) {
     fun records(): List<Record> {
+        TODO()
+    }
+
+    fun record(id: String): Record? {
+        TODO()
+    }
+
+    /**
+     * Deletes a record and all associated executions.
+     */
+    fun delete(recordId: String): Boolean {
+        val x = transaction(database) {
+            true
+        }
+        return x
+    }
+
+    fun create(record: RecordCreate): String {
+        TODO()
+    }
+
+    fun update(record: RecordUpdate): Boolean {
         TODO()
     }
 }
