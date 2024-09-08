58bc32a8365de4eb770583b07363864e7e87f117
tino <kpetras@tutanota.com>
2024-09-08 16:49:01 +0200

    implemented GraphQLApplication

--- backend/src/main/kotlin/com/craw/application/GraphQLApplication.kt
+++ backend/src/main/kotlin/com/craw/application/GraphQLApplication.kt
@@ -1,5 +1,26 @@
 package com.craw.application
 
+import com.craw.schema.graphql.Node
 import com.craw.schema.graphql.Query
+import com.craw.schema.graphql.WebPage
+import com.craw.translator.GraphQLTranslator
+import com.expediagroup.graphql.generator.scalars.ID
 
-interface GraphQLApplication : Query
+class GraphQLApplication(private val translator: GraphQLTranslator, private val repository: Repository) : Query {
+    override fun websites(): List<WebPage> {
+        val records = repository.records()
+        return records.map { record -> translator.translate(record) }
+    }
+
+    override fun nodes(webPages: List<ID>): List<Node> {
+        val ids = webPages.map { it.value }.toSet()
+        val records = repository.records()
+        return records
+            .filter { record -> record.recordId in ids }
+            .flatMap { record ->
+                val owner = translator.translate(record)
+                record.executions.map { execution -> translator.translate(owner, execution) }
+            }
+    }
+}
+
---
+++ backend/src/main/kotlin/com/craw/application/Repository.kt
@@ -0,0 +1,9 @@
+package com.craw.application
+
+import com.craw.schema.internal.Record
+
+class Repository {
+    fun records(): List<Record> {
+        TODO()
+    }
+}
