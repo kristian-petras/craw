5a1bd90cdf32f31a864d1f0f6bfb666aa4a4a5dc
tino <kpetras@tutanota.com>
2024-09-08 15:05:51 +0200

    added graphql translator

---
+++ backend/src/main/kotlin/com/craw/GraphQLTranslator.kt
@@ -0,0 +1,54 @@
+package com.craw
+
+import com.craw.schema.graphql.Node
+import com.craw.schema.graphql.WebPage
+import com.craw.schema.internal.Crawl
+import com.craw.schema.internal.Execution
+import com.craw.schema.internal.Record
+import com.expediagroup.graphql.generator.scalars.ID
+
+class GraphQLTranslator {
+    fun translate(record: Record): WebPage = WebPage(
+        identifier = ID(record.recordId),
+        label = record.label,
+        url = record.baseUrl,
+        regexp = record.regexp,
+        tags = record.tags,
+        active = record.active,
+    )
+
+    fun translate(owner: WebPage, execution: Execution): Node = when (execution) {
+        is Execution.Completed -> translate(owner, execution.crawl)
+        is Execution.Running -> translate(owner, execution.crawl)
+        is Execution.Scheduled -> Node(
+            title = null,
+            url = owner.url,
+            crawlTime = null,
+            links = emptyList(),
+            owner = owner
+        )
+    }
+
+    private fun translate(owner: WebPage, crawl: Crawl): Node = when (crawl) {
+        is Crawl.Completed -> Node(
+            title = crawl.title,
+            url = crawl.url,
+            crawlTime = crawl.crawlTime.toString(),
+            links = crawl.crawls.map { translate(owner, it) },
+            owner = owner
+        )
+
+        is Crawl.Running, is Crawl.Pending, is Crawl.Invalid -> Node(
+            title = null,
+            url = crawl.url,
+            crawlTime = null,
+            links = emptyList(),
+            owner = owner
+        )
+    }
+
+    private fun Crawl.toTitle(): String? = when (this) {
+        is Crawl.Completed -> title
+        is Crawl.Running, is Crawl.Invalid, is Crawl.Pending -> null
+    }
+}
