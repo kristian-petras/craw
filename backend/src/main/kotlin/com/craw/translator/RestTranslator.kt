b0d5f65b6ca49b117108444cfdfef8919c5da6e1
tino <kpetras@tutanota.com>
2024-09-08 18:48:46 +0200

    added CRUD translations to rest translator

--- backend/src/main/kotlin/com/craw/translator/RestTranslator.kt
+++ backend/src/main/kotlin/com/craw/translator/RestTranslator.kt
@@ -3,12 +3,16 @@ package com.craw.translator
 import com.craw.schema.internal.Crawl
 import com.craw.schema.internal.Execution
 import com.craw.schema.internal.Record
+import com.craw.schema.internal.RecordCreate
+import com.craw.schema.internal.RecordDelete
+import com.craw.schema.internal.RecordUpdate
 import com.craw.schema.rest.WebsiteExecution
 import com.craw.schema.rest.WebsiteRecord
+import com.craw.schema.rest.WebsiteRecordCreate
+import com.craw.schema.rest.WebsiteRecordDelete
+import com.craw.schema.rest.WebsiteRecordUpdate
 
 class RestTranslator {
-    fun translate(records: List<Record>): List<WebsiteRecord> = records.map { record -> translate(record) }
-
     fun translate(record: Record): WebsiteRecord = WebsiteRecord(
         recordId = record.recordId,
         url = record.baseUrl,
@@ -18,6 +22,29 @@ class RestTranslator {
         active = record.active,
         tags = record.tags,
         executions = record.executions.map { execution -> translate(execution) }
+    )
+
+    fun translate(recordCreate: WebsiteRecordCreate): RecordCreate = RecordCreate(
+        baseUrl = recordCreate.url,
+        regexp = recordCreate.regexp,
+        periodicity = recordCreate.periodicity,
+        label = recordCreate.label,
+        active = recordCreate.active,
+        tags = recordCreate.tags
+    )
+
+    fun translate(recordDelete: WebsiteRecordDelete): RecordDelete = RecordDelete(
+        recordId = recordDelete.recordId
+    )
+
+    fun translate(recordUpdate: WebsiteRecordUpdate): RecordUpdate = RecordUpdate(
+        recordId = recordUpdate.recordId,
+        baseUrl = recordUpdate.url,
+        regexp = recordUpdate.regexp,
+        periodicity = recordUpdate.periodicity,
+        label = recordUpdate.label,
+        active = recordUpdate.active,
+        tags = recordUpdate.tags
     )
 
     private fun translate(execution: Execution): WebsiteExecution = when (execution) {
