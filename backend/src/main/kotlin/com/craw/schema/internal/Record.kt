3fa78471438ef604450ea58e79e46fbbfb95a5c8
tino <kpetras@tutanota.com>
2024-09-08 17:33:41 +0200

    added record internal manipulation data classes

--- backend/src/main/kotlin/com/craw/schema/internal/Record.kt
+++ backend/src/main/kotlin/com/craw/schema/internal/Record.kt
@@ -10,3 +10,26 @@ data class Record(
     val tags: List<String>,
     val executions: List<Execution>,
 )
+
+data class RecordCreate(
+    val baseUrl: String,
+    val regexp: String,
+    val periodicity: String,
+    val label: String,
+    val active: Boolean,
+    val tags: List<String>,
+)
+
+data class RecordDelete(
+    val recordId: String,
+)
+
+data class RecordUpdate(
+    val recordId: String,
+    val baseUrl: String,
+    val regexp: String,
+    val periodicity: String,
+    val label: String,
+    val active: Boolean,
+    val tags: List<String>,
+)
