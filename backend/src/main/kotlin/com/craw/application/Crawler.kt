cb1472dbfbc8380032c7e570f533a5f42be5491e
tino <kpetras@tutanota.com>
2024-09-14 11:25:41 +0200

    crawler finished

--- backend/src/main/kotlin/com/craw/application/Crawler.kt
+++ backend/src/main/kotlin/com/craw/application/Crawler.kt
@@ -1,100 +1,120 @@
 package com.craw.application
 
-import com.craw.schema.database.CrawlType
+import com.craw.application.Parser.ParseResult
 import com.craw.schema.internal.Crawl
+import com.craw.utility.TimeProvider
+import com.craw.utility.logger
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.channelFlow
-import kotlinx.datetime.Instant
-import org.jsoup.Jsoup
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import com.craw.utility.TimeProvider
-import java.net.URI
-import kotlin.time.measureTime
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
 
 class Crawler(
     private val timeProvider: TimeProvider,
     private val repository: Repository,
+    private val parser: Parser,
     private val client: HttpClient,
-    private val dispatcher: CoroutineDispatcher,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) {
+
+    private val updateChannel = Channel<Unit>(Channel.UNLIMITED)
+
     fun crawl(executionId: String, url: String, regex: Regex): Flow<Crawl> = channelFlow {
-        val root = repository.create(CrawlCreate(executionId, url, null))
+        val root = repository.createCrawl(executionId, url, null)
+        updateChannel.send(Unit)
+        logger.info("[${root.crawlId}] Created root crawl for $url and executionId $executionId with regex $regex")
+
+        val sendJob = async {
+            updateChannel
+                .consumeAsFlow()
+                .mapNotNull { repository.getCrawl(root.crawlId) }
+                .onEach { logger.info("[${it.crawlId}] Sending updated state to re-render") }
+                .collect(::send)
+        }
 
         process(
+            scope = this,
             executionId = executionId,
-            parentId = null,
+            parentId = root.crawlId,
             crawl = root,
-            regex = regex,
-            activateCallback = { send(it) },
-            completeCallback = { send(it) }
+            regex = regex
         )
+
+        logger.info("[${root.crawlId}] Root crawl finished")
+        updateChannel.close()
+        sendJob.await()
     }
 
     private suspend fun process(
+        scope: CoroutineScope,
         executionId: String,
-        parentId: String?,
+        parentId: String,
         crawl: Crawl.Pending,
-        regex: Regex,
-        activateCallback: suspend (Crawl.Running) -> Unit,
-        completeCallback: suspend (Crawl.Completed) -> Unit
+        regex: Regex
     ) {
-        val activate = repository.activate(CrawlActivate(crawl.crawlId, timeProvider.now()))
-        activateCallback(activate)
+        val started = repository.startCrawl(crawl.crawlId, timeProvider.now())
+        logger.info("[${started.crawlId}] Starting crawl for ${started.url} at ${started.start}")
+        updateChannel.send(Unit)
 
-        val (title, matches, rest) = fetchLinks(crawl.url, regex)
+        when (val fetched = fetchLinks(crawl.url, regex)) {
+            is ParseResult.Failure -> {
+                repository.invalidateCrawl(
+                    crawlId = crawl.crawlId,
+                    error = fetched.message,
+                )
+                updateChannel.send(Unit)
+                return
+            }
 
-        val pending = matches.map { repository.create(CrawlCreate(executionId, it, parentId)) }
-        val invalid = rest.map { repository.invalidate(CrawlNotMatched(executionId, it)) }
+            is ParseResult.Success -> {
+                val (title, matches, rest) = fetched
+                logger.info("[${started.crawlId}] Found ${matches.size} matches and ${rest.size} invalid links")
 
-        var completed = repository.complete(
-            crawlComplete = CrawlComplete(
-                crawlId = crawl.crawlId,
-                title = title,
-                end = timeProvider.now(),
-                pending = pending,
-                invalid = invalid
-            )
-        )
-        completeCallback(completed)
-
-        pending.forEach { child ->
-            process(
-                executionId = executionId,
-                parentId = completed.crawlId,
-                crawl = child,
-                regex = regex,
-                activateCallback = {
-                    completed = repository.update(CrawlUpdateRunning(completed.crawlId, it))
-                    completeCallback(completed)
-                },
-                completeCallback = {
-                    completed = repository.update(CrawlUpdate(completed.crawlId, it))
-                    completeCallback(completed)
+                val pending = matches.map { repository.createCrawl(executionId, it, parentId) }.onEach {
+                    logger.info("[${it.crawlId}] Created crawl for ${it.url} with parent $parentId")
                 }
-            )
+                rest.map { repository.createInvalidCrawl(executionId, it, parentId) }.onEach {
+                    logger.info("[${it.crawlId}] Created invalid crawl for ${it.url} with parent $parentId")
+                }
+
+                val completed = repository.completeCrawl(
+                    crawlId = crawl.crawlId,
+                    title = title,
+                    end = timeProvider.now()
+                )
+                logger.info("[${completed.crawlId}] Completed crawl for ${completed.url} at ${completed.end}")
+                updateChannel.send(Unit)
+
+                pending.map { child ->
+                    scope.async(dispatcher) {
+                        process(
+                            scope = scope,
+                            executionId = executionId,
+                            parentId = completed.crawlId,
+                            crawl = child,
+                            regex = regex,
+                        )
+                    }
+                }.awaitAll()
+            }
         }
     }
 
-    private data class ParseResult(val title: String?, val matches: List<String>, val rest: List<String>)
 
     private suspend fun fetchLinks(url: String, regex: Regex): ParseResult {
-        val payload = client.get(url).bodyAsText() // TODO handle failure
-        val document = Jsoup.parse(payload)
-        val title = document.title()
-        val links = document.select("a[href]").map { link -> link.attr("href") }
-        val (matches, rest) = links.partition { regex.matches(it) }
-        return ParseResult(title, matches, rest)
+        val payload = client.get(url)
+        if (payload.status.isSuccess()) {
+            return parser.parse(payload.bodyAsText(), regex)
+        } else {
+            logger.error("Failed to fetch $url with status ${payload.status}")
+            return ParseResult.Failure(payload.status.value, payload.status.description)
+        }
     }
 
     companion object {
         private val logger = logger<Crawler>()
     }
 }
-
-inline fun <reified T> logger(): Logger = LoggerFactory.getLogger(T::class.java)
